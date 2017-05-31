e4b8d0f53704d347fad9ed8dae1cea95ec2cdff6
Juan Sebastian Moriones <juanchod.9@hotmail.com>
2017-05-31 10:35:26 -0500

    change view MorePage

--- src/pages/more/more.ts
+++ src/pages/more/more.ts
@@ -12,26 +12,40 @@ import { TransactionGoodPage } from '../transactiongood/transaction';
  */
 @IonicPage()
 @Component({
-  selector: 'page-more',
-  templateUrl: 'more.html',
+	selector: 'page-more',
+	templateUrl: 'more.html',
 })
 export class MorePage {
-  public user:any={name:null,email:null,cedula:null};
-  public tran: any = {estopa:null,rallas:null,kilo:null};
-  public productor: any;
-  constructor(public alertCtrl: AlertController,public navCtrl: NavController, public navParams: NavParams) { 
-  this.user.name=navParams.get('name');
-  this.user.email=navParams.get('email');
-  this.user.cedula=navParams.get('cedula');
-  this.productor = navParams.get('productor');
-  console.log(this.productor);
-   }
-  submit(tran:any){
-    console.log(this.tran);
-  }
-  ionViewDidLoad() {
-  }
-  goodProcess(){
-    this.navCtrl.push(TransactionGoodPage, {});
-  }
+	public user:any={name:null,email:null,cedula:null};
+	public tran: any = {estopa:null,rallas:null,kilo:null, kilograms:null};
+	public productor: any;
+	public bl: boolean;
+	constructor(public alertCtrl: AlertController,public navCtrl: NavController, public navParams: NavParams) { 
+			this.user.name=navParams.get('name');
+			this.user.email=navParams.get('email');
+			this.user.cedula=navParams.get('cedula');
+			this.productor = navParams.get('productor');
+			console.log(this.productor);
+			//console.log(this.productor.kilograms);
+			if(this.productor == undefined){
+				console.log("Bien");
+				this.bl = false;
+				console.log(this.bl);
+			}else{
+				this.bl = true;
+				console.log("Falso");
+				this.tran.estopa = this.productor.estopa;
+				this.tran.rallas = this.productor.raya;
+				this.tran.kilo = this.productor.kilo;
+				this.tran.kilograms = this.productor.kilograms;
+			}
+	 }
+	submit(tran:any){
+		console.log(this.tran);
+	}
+	ionViewDidLoad() {
+	}
+	goodProcess(){
+		this.navCtrl.push(TransactionGoodPage, {});
+	}
 }
--- src/providers/login.ts
+++ src/providers/login.ts
@@ -5,99 +5,99 @@ import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
 import 'rxjs/add/operator/map';
 
 /*
-  Generated class for the Login provider.
+	Generated class for the Login provider.
 
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
+	See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+	for more info on providers and Angular 2 DI.
 */
 //http://35.184.34.17/api/transactions
 @Injectable()
 export class LoginProvider {
-  db:SQLite=null;
-  open:any=null;
-  constructor(public http: Http) {
-    this.db= new SQLite();
-  }
-  validar(user:any){
-  	console.log(user);
-  	
-  	return this.http.post("http://35.184.34.17/api/receiver/sign-in", user)
-  	.map((response:Response)=>response.json());
-  }
+	db:SQLite=null;
+	open:any=null;
+	constructor(public http: Http) {
+		this.db= new SQLite();
+	}
+	validar(user:any){
+		console.log(user);
+		
+		return this.http.post("http://35.184.34.17/api/receiver/sign-in", user)
+		.map((response:Response)=>response.json());
+	}
 
-  getUsers(){	
-  	return this.http.get("http://35.184.34.17/api/receivers")
-  	.map((response:Response)=>response.json());
-  }
-  reposGithub(){
-    return this.http.get("https://api.github.com/users/codigofacilito/repos");
-  }
-  validarCodeUser(code:any){
-    console.log(code);
-    
-    return this.http.post("http://35.184.34.17/api/user-code", code)
-    .map((response:Response)=>response.json());
-  }
-  createUser(user:any){
-    
-    return this.http.post("http://35.184.34.17/api/user", user)
-    .map((response:Response)=>response.json());
-  }
-  openDataBase(){
-    return this.db.create({
+	getUsers(){	
+		return this.http.get("http://35.184.34.17/api/receivers")
+		.map((response:Response)=>response.json());
+	}
+	reposGithub(){
+		return this.http.get("https://api.github.com/users/codigofacilito/repos");
+	}
+	validarCodeUser(code:any){
+		console.log(code);
+		
+		return this.http.post("http://35.184.34.17/api/user-code", code)
+		.map((response:Response)=>response.json());
+	}
+	createUser(user:any){
+		
+		return this.http.post("http://35.184.34.17/api/user", user)
+		.map((response:Response)=>response.json());
+	}
+	openDataBase(){
+		return this.db.create({
 
-      name: 'data.db',
-      location: 'default' // the location field is required
-    }).then((db: SQLiteObject) => {
-      this.open=db;
-      db.executeSql('CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT,name VARCHAR(32), images VARCHAR(15), email VARCHAR(25))', {})
-        .then(() => console.log('Executed SQL'))
-        .catch(e => console.log(e));
-    }, (err) => {
-      console.error('Unable to open database: ', err);
-    });
-  }
-  saveUser(user:any){
-    this.db.create({
-      name: 'data.db',
-      location: 'default' // the location field is required
-    }).then((db: SQLiteObject) => {
-       console.log('Se Creo el usuario');
-       console.log(user);
-      db.executeSql('INSERT OR REPLACE INTO users(name,email,images)  VALUES (?,?,?)', [user.name, user.img, user.email])
-        .then(() => console.log('Executed SQL'))
-        .catch(e => console.log(e));
-    }, (err) => {
-      console.error('Unable to open database: ', err);
-    });
-  }
-  checkUser(){
-    return this.db.create({
-      name: 'data.db',
-      location: 'default' // the location field is required
-    }).then((db: SQLiteObject) => {
-       return db.executeSql('SELECT * FROM users ORDER BY id ASC LIMIT 1',[])
-        .then(user=> {
-          if(user.rows.length>0) {            
-            return Promise.resolve(true);
-          }else{
-            return Promise.resolve(false);
-          }
-        });
-    }, (err) => {
-      console.error('Unable to open database: ', err);
-    });
-  }
-  closeSesion(){
-    return this.db.create({
-      name: 'data.db',
-      location: 'default' // the location field is required
-    }).then((db: SQLiteObject) => {
-      return db.executeSql('DELETE FROM users',[]);
-    })
-  }
-  getTransaction(){
-    return this.http.get("http://35.184.34.17/api/transactions")
-      .map((res: Response)=>res.json());
-  }
+			name: 'data.db',
+			location: 'default' // the location field is required
+		}).then((db: SQLiteObject) => {
+			this.open=db;
+			db.executeSql('CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT,name VARCHAR(32), images VARCHAR(15), email VARCHAR(25))', {})
+				.then(() => console.log('Executed SQL'))
+				.catch(e => console.log(e));
+		}, (err) => {
+			console.error('Unable to open database: ', err);
+		});
+	}
+	saveUser(user:any){
+		this.db.create({
+			name: 'data.db',
+			location: 'default' // the location field is required
+		}).then((db: SQLiteObject) => {
+			 console.log('Se Creo el usuario');
+			 console.log(user);
+			db.executeSql('INSERT OR REPLACE INTO users(name,email,images)  VALUES (?,?,?)', [user.name, user.img, user.email])
+				.then(() => console.log('Executed SQL'))
+				.catch(e => console.log(e));
+		}, (err) => {
+			console.error('Unable to open database: ', err);
+		});
+	}
+	checkUser(){
+		return this.db.create({
+			name: 'data.db',
+			location: 'default' // the location field is required
+		}).then((db: SQLiteObject) => {
+			 return db.executeSql('SELECT * FROM users ORDER BY id ASC LIMIT 1',[])
+				.then(user=> {
+					if(user.rows.length>0) {            
+						return Promise.resolve(true);
+					}else{
+						return Promise.resolve(false);
+					}
+				});
+		}, (err) => {
+			console.error('Unable to open database: ', err);
+		});
+	}
+	closeSesion(){
+		return this.db.create({
+			name: 'data.db',
+			location: 'default' // the location field is required
+		}).then((db: SQLiteObject) => {
+			return db.executeSql('DELETE FROM users',[]);
+		})
+	}
+	getTransaction(){
+		return this.http.get("http://35.184.34.17/api/transactions")
+			.map((res: Response)=>res.json());
+	}
 }
